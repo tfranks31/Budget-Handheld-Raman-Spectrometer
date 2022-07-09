8485a0a90ef709ede8cfd79535137bf2e90edb87
Tyler <tcfranks31@gmail.com>
2022-07-09 17:25:04 -0400

    Created joystick module implementation

---
+++ Src/HRS/src/modules/Joystick.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include <modules/Joystick.hpp>
+
+Joystick::Joystick(uint8_t xPinNum, uint8_t yPinNum, uint8_t buttonPinNum)
+{
+    this->xPinNum = xPinNum;
+    this->yPinNum = yPinNum;
+    this->buttonPinNum = buttonPinNum;
+    clearButtonPress();
+}
+
+void Joystick::init()
+{
+    // Analog pins do not need to be declared as inputs
+    pinMode(buttonPinNum, INPUT);
+}
+
+uint16_t Joystick::getXPos()
+{
+    return analogRead(xPinNum);
+}
+
+uint16_t Joystick::getYPos()
+{
+    return analogRead(yPinNum);
+}
+
+bool Joystick::getButtonPressed()
+{
+    return wasButtonPressed;
+}
+
+void Joystick::clearButtonPress()
+{
+    wasButtonPressed = false;
+}
+
+void Joystick::handleButtonPress()
+{
+    wasButtonPressed = true;
+}
